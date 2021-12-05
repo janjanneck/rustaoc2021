b2eab07aa0aa16d10814d1a5649bc827cd5a4954
Jan Robert <jan.janneck@outlook.com>
2021-12-05 18:52:08 +0100

    day5 pt1

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 //use std::io;
 use std::fs;
 
 fn main() {
-    day4::run(read_input("day4/input.txt"));
+    day5::run(read_input("day5/input.txt"));
 }
 
 fn read_input(filename: &str) -> String {
